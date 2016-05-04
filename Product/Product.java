786ac66e4be3a76316c06581e91cfe51e4a052cc
alex_hanson <alex_hanson@office.com>
2016-05-04 14:06:46 +0200

    add price

--- Product/Product.java
+++ Product/Product.java
@@ -1,6 +1,7 @@
 public class Product
 {
 	private String 	name;	// "Coca Cola"
+	private float	price;	// 10
 	
 	public Product ()
 	{
@@ -14,5 +15,15 @@ public class Product
 	public void setName (String name)
 	{
 		this.name = name;
+	}
+	
+	public String getPrice ()
+	{
+		return this.price;
+	}
+	
+	public void setPrice (float price)
+	{
+		this.price = price;
 	}
 }
