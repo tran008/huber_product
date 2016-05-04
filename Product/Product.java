23e3de75488b64ad08d7261c01181f245ace14d7
alex_hanson <alex_hanson@office.com>
2016-05-04 14:25:10 +0200

    add id; change LinkedList

--- Product/Product.java
+++ Product/Product.java
@@ -2,6 +2,7 @@ public class Product
 {
 	private String 	name;	// "Coca Cola"
 	private float	price;	// 10
+	private int 	id;
 	
 	public Product ()
 	{
@@ -25,5 +26,15 @@ public class Product
 	public void setPrice (float price)
 	{
 		this.price = price;
+	}
+	
+	public int getId ()
+	{
+		return this.id;
+	}
+	
+	public void setId (int id)
+	{
+		this.id = id;
 	}
 }
